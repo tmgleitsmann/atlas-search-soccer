3ae02f64b956598124294226184ae4cebbd1a0f8
YouOldMaid <karen.huaulme@gmail.com>
2022-05-14 21:29:13 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// Dependencies
+import React from "react";
+
+// Styles
+import "./tailwind.output.css";
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-yellow-300">
+      <h2 className="text-center text-4xl font-bold text-blue-900">
+        TailwindCSS + React
+      </h2>
+    </div>
+  );
+};
+
+export default App;
